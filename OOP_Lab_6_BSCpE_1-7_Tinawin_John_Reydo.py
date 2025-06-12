927a2363972958097de364109b55f4de078ff0fc
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2025-06-12 21:48:26 +0800

    Added getter methods to access data methods

---
+++ OOP_Lab_6_BSCpE_1-7_Tinawin_John_Reydo.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+    
+    # Getter methods
+    def get_year_model(self):
+        return self.__year_model
+    
+    def get_make(self):
+        return self.__make
+    
+    def get_speed(self):
+        return self.__speed
