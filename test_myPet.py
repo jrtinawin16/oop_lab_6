1e1a6f2cea41fbcb414da4c188eec1794662a338
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2025-06-17 09:27:55 +0800

    Created Pet objects

--- test_myPet.py
+++ test_myPet.py
@@ -2,3 +2,8 @@ from myPet import Pet
 name = input("Enter your pet's name: ")
 animal_type = input("Enter type of animal (Dog, Bird, Fish): ")
 age = input("Enter your pet's age: ")
+
+my_pet = Pet()
+my_pet.set_name(name)
+my_pet.set_animal_type(animal_type)
+my_pet.set_age(age)
