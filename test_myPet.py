c786751192ab50fb1e2741d749d05a324b31fda6
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2025-06-17 09:45:32 +0800

    Fixed errors in getting age

--- test_myPet.py
+++ test_myPet.py
@@ -1,6 +1,6 @@
 from myPet import Pet
 name = input("Enter your pet's name: ")
-animal_type = input("Enter type of animal (Dog, Bird, Fish): ")
+animal_type = input("Enter type of animal (e.g. Dog, Bird, Fish): ")
 age = input("Enter your pet's age: ")
 
 my_pet = Pet()
@@ -11,4 +11,4 @@ my_pet.set_age(age)
 print("\n🐱🐔 Pet Information 🐶🐟")
 print(f"Name: {my_pet.get_name()}")
 print(f"Type: {my_pet.get_animal_type()}")
-print(f"Age: {my_pet.get_age}")
+print(f"Age: {my_pet.get_age()}")
